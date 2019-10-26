6ea305c7899d8d43a02399dc637b3de80787a686
Leslie Chan <leslie.jw.chan@gmail.com>
2019-10-26 23:44:02 +0800

    Added Observer Pattern.

--- Observer/Program.cs
+++ Observer/Program.cs
@@ -2,11 +2,25 @@
 
 namespace Observer
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
+            // the subject.
+            Stock companyStock = new Stock();
+
+            // the observers.
+            Person personA = new Person("Warren");
+            Person personB = new Person("Thomas");
+
+            // watch the stock prices.
+            companyStock.Register(personA);
+            companyStock.Register(personB);
+
+            // state changes.
+            companyStock.Value = 10.1;
+            companyStock.Value = 10.5;
+            companyStock.Value = 9.4;
         }
     }
 }
---
+++ Observer/Stock.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+namespace Observer
+{
+    /// <summary>
+    /// A type of security that signifies proportionate ownership in the issuing
+    /// corporation.
+    /// </summary>
+    public class Stock : ISubject
+    {
+        private List<IObserver> _observers = new List<IObserver>();
+        private double _value;
+
+        /// <summary>
+        /// Initializes an isntance of the <see cref="Stock"/> class.
+        /// </summary>
+        public Stock()
+        {
+            _value = 0.0;
+        }
+
+        /// <summary>
+        /// The value.
+        /// </summary>
+        public double Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (value >= 0.0)
+                {
+                    _value = value;
+                    Notify();
+                }
+            }
+        }
+
+        /// <summary>
+        /// register an observer to be notified when this object's state 
+        /// changes.
+        /// </summary>
+        public void Register(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        /// <summary>
+        /// unregister an observer to stop it being notified of this object's
+        /// state changes.
+        /// </summary>
+        public void Unregister(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        /// <summary>
+        /// logic to notify the registered observers.
+        /// </summary>
+        public void Notify()
+        {
+            foreach (IObserver observer in _observers)
+            {
+                observer.Update(this);
+            }
+        }
+    }
+}
