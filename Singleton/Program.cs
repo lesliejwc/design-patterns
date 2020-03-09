97f4248fc6fbe9b161c594b56bc33ec7962063d1
Leslie Chan <leslie@idearlabs.com>
2020-03-09 14:38:14 +0800

    Added singleton pattern implementation

---
+++ Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton inst1 = Singleton.GetInstance();
+            Singleton inst2 = Singleton.GetInstance();
+
+            if (inst1 == inst2)
+            {
+                Console.WriteLine("Same instance");
+            }
+            else
+            {
+                Console.WriteLine("Not the same");
+            }
+        }
+    }
+}
